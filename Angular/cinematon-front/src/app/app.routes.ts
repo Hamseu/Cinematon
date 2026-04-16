a87b8d5f90000bfecfc7690fdb0a6889395e36f5
webuser <nurbek120105@gmail.com>
2026-04-16 10:46:54 +0500

    front-pages

--- Angular/cinematon-front/src/app/app.routes.ts
+++ Angular/cinematon-front/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { TicketComponent } from './pages/ticket/ticket';
+import { Profile } from './pages/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'ticket/:id', component: TicketComponent },
+  { path: 'profile', component: Profile },
+];
