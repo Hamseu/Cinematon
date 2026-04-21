681d4a917eae1a1e6dd6eab64d8159a3e23cf90f
webuser <nurbek120105@gmail.com>
2026-04-21 21:38:34 +0500

    add

--- Angular/cinematon-front/src/app/pages/ticket/ticket.ts
+++ Angular/cinematon-front/src/app/pages/ticket/ticket.ts
@@ -1,20 +1,6 @@
 import { Component, inject, computed } from '@angular/core';
 import { ActivatedRoute, RouterModule , RouterOutlet, RouterLink, RouterLinkActive} from '@angular/router';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-@Component({
-  selector: 'app-ticket',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './ticket.html',
-})
-export class TicketComponent {
-  filmId!: number;
-
-  constructor(private route: ActivatedRoute) {
-    this.filmId = Number(this.route.snapshot.paramMap.get('id'));
-  }
-=======
 import { Httpserf } from '../../httpserf';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { switchMap } from 'rxjs';
@@ -66,7 +52,6 @@ export class Ticket {
 
 
   
->>>>>>> 46e2ca113ed82e98d9236676b06ee1ae1a5e7d8a
   buyTicket() {
     alert('Ticket successfully purchased!');
   }
